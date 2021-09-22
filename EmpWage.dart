9ab00d646468109da16f3ee90053252aa8c19969
karuna <karunakakade8@gmail.com>
2021-09-22 15:37:43 +0530

    Added: Calculated daily wage

--- EmpWage.dart
+++ EmpWage.dart
@@ -1,3 +1,28 @@
+import 'dart:math';
+
 void main() {
-  print("Welcome to Employee Wage");
+  EmpWage emp = new EmpWage();
+  emp.display();
+}
+
+class EmpWage {
+  // Random random = new Random();
+  int fullTime = 1;
+  int ratePerHr = 20;
+  int salary = 0;
+  int empCheck = new Random().nextInt(2);
+
+  void display() {
+    if (empCheck == fullTime) {
+      print("Employee is present");
+      int empHrs = 8;
+      salary = empHrs * ratePerHr;
+      print("Employee Wage: $salary");
+    } else {
+      print("Employee is absent");
+      int empHrs = 0;
+      salary = empHrs * ratePerHr;
+      print("Employee Wage: $salary");
+    }
+  }
 }
