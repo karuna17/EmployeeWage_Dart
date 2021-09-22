cc68a5a27a980ccdc881ba780743debb34544658
karuna <karunakakade8@gmail.com>
2021-09-22 16:02:00 +0530

    Added: Employee Wage using switch case

--- EmpWage.dart
+++ EmpWage.dart
@@ -7,28 +7,35 @@ void main() {
 
 class EmpWage {
   // Random random = new Random();
-  int fullTime = 1;
-  int partTime = 2;
+  static const int fullTime = 1;
+  static const int partTime = 2;
   int ratePerHr = 20;
   int salary = 0;
+  int empHrs = 0;
+
   int empCheck = new Random().nextInt(3);
 
   void display() {
-    if (empCheck == fullTime) {
-      print("Employee is FullTime");
-      int empHrs = 8;
-      salary = empHrs * ratePerHr;
-      print("Employee Wage: $salary");
-    } else if (empCheck == partTime) {
-      print("Employee is PartTime");
-      int empHrs = 4;
-      salary = empHrs * ratePerHr;
-      print("Employee Wage: $salary");
-    } else {
-      print("Employee is absent");
-      int empHrs = 0;
-      salary = empHrs * ratePerHr;
-      print("Employee Wage: $salary");
+    switch (empCheck) {
+      case fullTime:
+        print("Employee is FullTime");
+        int empHrs = 8;
+        salary = empHrs * ratePerHr;
+        print("Employee Wage: $salary");
+        break;
+
+      case partTime:
+        print("Employee is PartTime");
+        int empHrs = 4;
+        salary = empHrs * ratePerHr;
+        print("Employee Wage: $salary");
+        break;
+
+      default:
+        print("Employee is absent");
+        int empHrs = 0;
+        salary = empHrs * ratePerHr;
+        print("Employee Wage: $salary");
     }
   }
 }
