3c7f60bc8b96f06c8d62356e5685b8f2bc21bdeb
karuna <karunakakade8@gmail.com>
2021-09-22 16:13:53 +0530

    Added: Employee Wage of month

--- EmpWage.dart
+++ EmpWage.dart
@@ -1,3 +1,42 @@
+import 'dart:math';
+
 void main() {
-  print("Welcome to Employee Wage");
+  EmpWage emp = new EmpWage();
+  emp.display();
+}
+
+class EmpWage {
+  // Random random = new Random();
+  static const int fullTime = 1;
+  static const int partTime = 2;
+  static int ratePerHr = 20;
+  static int TotalDays = 30;
+  int salary = 0;
+  int empHrs = 0;
+  int totalSalary = 0;
+
+  int empCheck = new Random().nextInt(3);
+
+  void display() {
+    for (int count = 1; count <= TotalDays; count++) {
+      switch (empCheck) {
+        case fullTime:
+          int empHrs = 8;
+          salary = empHrs * ratePerHr;
+          break;
+
+        case partTime:
+          int empHrs = 4;
+          salary = empHrs * ratePerHr;
+          break;
+
+        default:
+          int empHrs = 0;
+          salary = empHrs * ratePerHr;
+      }
+
+      totalSalary += salary;
+    }
+    print("Total Employee Wage: $totalSalary");
+  }
 }
